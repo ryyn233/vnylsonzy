8eec50183493ddf68fdc00c851bc0a89d6d5d324
ryyn233 <gnjing52@gmail.com>
2025-03-24 21:30:57 +0700

    upload via termux

---
+++ config.js
@@ -0,0 +1,81 @@
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+
+// Global Settings
+global.setting = {
+  autoclear: false,
+  addReply: true, // Tambahkan balasan dengan thumbnail di pesan
+};
+
+// Owner Information
+global.owner = [
+  ['6285956337454', 'YannOfficial-ID', true],
+];
+
+// Bot Information
+global.info = {
+  nomerbot: '6289630388268', // nomor bot mu
+  pairingNumber: '6289630388268', // penting!! wajib ganti jadi nomor bot mu 
+  nameown: 'YannOfficial-ID',
+  nomerown: '6285956337454',
+  token: 'VynaaAI4m2n1o', // Token harus diisi agar bot dapat berjalan
+  packname: 'sticker by',
+  author: 'YannOfficial-ID',
+  namebot: 'Vynaa AI 10.21',
+  wm: 'made by YannOfficial',
+  stickpack: 'YT: YannOfficial-ID',
+  stickauth: 'S U B S C R I B E',
+};
+
+global.wait = '_M O H O N - T U N G G U_'; // Pesan saat menunggu
+global.maxwarn = 5;
+
+// URLs
+global.url = {
+  profil: 'https://files.catbox.moe/ijeati.jpg',
+  did: 'https://telegra.ph/file/fdc1a8b08fe63520f4339.jpg',
+  rules: 'https://telegra.ph/file/afcfa712bd09f4fcf027a.jpg',
+  thumbnail: 'https://files.catbox.moe/ijeati.jpg',
+  thumb: 'https://files.catbox.moe/ijeati.jpg',
+  logo: 'https://telegra.ph/file/07428fea2fd4dccaab65f.jpg',
+  unReg: 'https://telegra.ph/file/ef02d1fdd59082d05f08d.jpg',
+  registrasi: 'https://itzpire.com/file/6ead5b50254b.jpg',
+  confess: 'https://telegra.ph/file/03cabea082a122abfa5be.jpg',
+  akses: 'https://telegra.ph/file/6c7b9ffbdfb0096e1db3e.jpg',
+  wel: 'https://telegra.ph/file/9dbc9c39084df8691ebdd.mp4', // Welcome GIF
+  bye: 'https://telegra.ph/file/1c05b8c019fa525567d01.mp4', // Goodbye GIF
+  sound: 'https://media.vocaroo.com/mp3/1awgSZYHXP3B', // Audio menu
+  sig: '',
+  sgh: '',
+  sgc: 'https://whatsapp.com/channel/0029VauPJAhEVccHHykhFR1U',
+};
+
+// Payment Information
+global.payment = {
+  psaweria: 'https://saweria.co/',
+  ptrakterr: '-',
+  pdana: '',
+};
+
+// API Configuration
+global.api = {
+  btch: '_',
+  rose: '_',
+};
+global.APIs = {
+  btch: 'https://api.botcahx.eu.org',
+  rose: 'https://api.itsrose.rest',
+};
+global.APIKeys = {
+  'https://api.botcahx.eu.org': '_',
+  'https://api.itsrose.rest': '_',
+};
+
+// Watch for File Changes
+let file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  import(`${file}?update=${Date.now()}`);
+});
